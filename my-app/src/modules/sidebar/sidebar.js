c38ac8d566c5a3ac602f79a0006fbf6a0c7f8b26
YiFan Chen <yifanch3@andrew.cmu.edu>
2022-02-15 22:19:22 -0500

    fix: save current progress here

---
+++ my-app/src/modules/sidebar/sidebar.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './sidebar.module.css';
+
+export const SideBar = () => (
+    <div className={styles.container}>
+        {'this is a test'}
+    </div>
+);
